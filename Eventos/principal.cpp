470d91a5728f09f29a57a652473cbdcba5667547
Bernardo <125703112+b3rnard0p@users.noreply.github.com>
2023-10-16 20:27:16 -0300

    Add files via upload

---
+++ Eventos/principal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#define TAM 50
+using namespace std;
+
+#include "structs.h"
+
+#include "metodo.h"
+
+int main() {
+    Participante vetor[TAM];
+    int totalInscritos = conectarBase(vetor,TAM);
+    menu(vetor, TAM, totalInscritos);   
+    return 1;
+    
+}
