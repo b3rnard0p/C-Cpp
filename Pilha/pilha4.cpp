8604844404b9fbd99e23033770aff833b4ac3e94
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2024-03-18 18:36:06 -0300

    Add files via upload

---
+++ Pilha/pilha4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#include "pilha.h"
+
+int main() 
+{
+    Pilha *pilha = NULL;
+
+    pilha = inserir(10, pilha);
+    pilha = inserir(15, pilha);
+    pilha = inserir(5, pilha);
+
+    exibir(pilha);
+    cout << topo(pilha) << endl;
+
+    cout << localizado(6,pilha) << endl;
+
+    pilha = remover(pilha);
+    cout << "Pilha alterada" << endl;
+
+    exibir(pilha);
+
+    pilha = limpar(pilha);
+
+    cout << "Tentando exibir pilha destruida\n";
+    
+    exibir(pilha);
+
+    return 1;
+
+}
